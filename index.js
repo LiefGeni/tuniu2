b1c53c44ef2841cce36a26d47024efae44570283
LiefGeni <nanshenstudy@163.com>
2019-07-08 16:21:37 +0800

    小程序

---
+++ index.js
@@ -0,0 +1,30 @@
+define(function(require){
+	var $ = require("jquery");
+	var justep = require("$UI/system/lib/justep");
+	var ShellImpl = require('$UI/system/lib/portal/shellImpl');
+	require("$UI/demo/tuniu/appVersionChecker");
+	
+	var Model = function(){
+		this.callParent();
+		var shellImpl = new ShellImpl(this, {
+			"contentsXid" : "pages",
+			"pageMappings" : {
+				"main":{
+					url : '$UI/demo/tuniu/index_main.w'
+				},
+				"searchList":{
+					url : '$UI/demo/tuniu/searchList.w'
+				},
+				"detail":{
+					url : '$UI/demo/tuniu/detail.w'
+				}
+			}
+		});
+	};
+
+	Model.prototype.modelLoad = function(event){
+		justep.Shell.showPage("main");
+	};
+
+	return Model;
+});
